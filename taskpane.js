e28dc66715db7e283e1393c544cac055b2452453
b-arte-k <113290123+b-arte-k@users.noreply.github.com>
2026-05-15 13:18:02 +0200

    Add files via upload

---
+++ taskpane.js
@@ -0,0 +1,9 @@
+Office.onReady(() => {});
+
+async function colorCell() {
+  await Excel.run(async (context) => {
+    const range = context.workbook.getSelectedRange();
+    range.format.fill.color = "yellow";
+    await context.sync();
+  });
+}
